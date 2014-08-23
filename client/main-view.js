9fae02770bd895cd7edb2202739a5eb277b440d1
ggoodwin37 <ggoodwin37@gmail.com>
2014-08-22 20:58:31 -0700

    Adding in jquery. Making canvas full-window.

--- client/main-view.js
+++ client/main-view.js
@@ -1,9 +1,14 @@
+var $ = require('jquery');
 var View = require('ampersand-view');
 var templates = require('./templates');
 
 module.exports = View.extend({
 	template: templates.body,
 	autoRender: true,
-	initialize: function() {
+	render: function () {
+		this.renderWithTemplate();
+		var $canvas = $('[role="main-canvas"]');
+		console.log('canvas size is ' + $canvas.width() + ' x ' + $canvas.height());
+		return this;
 	}
 });
