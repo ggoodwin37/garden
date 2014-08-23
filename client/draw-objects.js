60582ce7f9e3ced91b4f968bf6843a63cd9e274a
ggoodwin37 <ggoodwin37@gmail.com>
2014-08-22 21:07:46 -0700

    Initial commit for draw-objects library.

---
+++ client/draw-objects.js
@@ -0,0 +1,8 @@
+
+var drawObjects = {
+	drawShip: function($canvas, cx, cy, rBounding, thetaDeg, thrusters) {
+		
+	}
+};
+
+module.exports = drawObjects;
--- client/main-view.js
+++ client/main-view.js
@@ -1,6 +1,7 @@
 var $ = require('jquery');
 var View = require('ampersand-view');
 var templates = require('./templates');
+var drawObjects = require('./draw-objects');
 
 module.exports = View.extend({
 	template: templates.body,
@@ -8,7 +9,19 @@ module.exports = View.extend({
 	render: function () {
 		this.renderWithTemplate();
 		var $canvas = $('[role="main-canvas"]');
+
+		// TODO: resize handler
 		console.log('canvas size is ' + $canvas.width() + ' x ' + $canvas.height());
+
+		// testing
+		var cx = $canvas.width() / 2;
+		var cy = $canvas.height() / 2;
+		var radius = 10;
+		var thetaDeg = 0;
+		var thrusters = false;
+
+		drawObjects.drawShip($canvas, cx, cy, radius, thetaDeg, thrusters);
+
 		return this;
 	}
 });
