638e24c2a5e27fd026774a8d73008bbbe4c099e6
gideong <gideong@yahoo-inc.com>
2014-08-22 18:23:14 -0700

    WIP app entry point.

--- client/app.js
+++ client/app.js
@@ -1,1 +1,13 @@
-window.alert('hello');
+var domready = require('domready');
+var MainView = require('./main-view');
+
+window.app = {
+	init: function() {
+		var self = this;
+		domready(function() {
+			self.view = new MainView({
+				el: document.body
+			});
+		});
+	}
+};
---
+++ client/main-view.js
@@ -0,0 +1,10 @@
+var View = require('ampersand-view');
+var templates = require('./templates');
+
+module.exports = View.extend({
+	template: templates.body,
+	autoRender: true,
+	intialize: function() {
+		window.alert('hello');
+	}
+});
