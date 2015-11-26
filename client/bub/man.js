f9382622fbbbf17bec0fc2b1ac3279a2a024c673
ggoodwin37 <ggoodwin37@gmail.com>
2015-11-26 14:30:23 -0800

    bubSrc

--- client/bub/man.js
+++ client/bub/man.js
@@ -1,15 +1,23 @@
 // Bub is the basic particle system in this game.
 //  BubMan manages a list of BubSrc's, each of which own their own individual Bubs.
 function BubMan() {
+	this.srcList = [];
 }
 
 BubMan.prototype.update = function(deltaMs) {
+	this.srcList.forEach(function(thisSrc) {
+		thisSrc.update(deltaMs);
+	});
 };
 
 BubMan.prototype.drawAll = function(ctx) {
+	this.srcList.forEach(function(thisSrc) {
+		thisSrc.draw(ctx);
+	});
 };
 
 BubMan.prototype.addSource = function(src) {
+	this.srcList.push(src);
 };
 
 module.exports = BubMan;
