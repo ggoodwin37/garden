7158c70906aeb933e421c355fffe1bfd74a3a03e
ggoodwin37 <ggoodwin37@gmail.com>
2015-11-27 08:44:22 -0800

    Kill TODO.

--- client/bub/src.js
+++ client/bub/src.js
@@ -1,7 +1,6 @@
 // represents a bub spawner. It has a concept of location (which can be updated), and possibly additional state.
 // since this is a visual effect that is typically tied to a game object, this will just have position, not velocity.
 // it can update its position to lock on to a game object (which itself may have velocity).
-// TODO: all bubsrcs act the same for now, might want to make them have unique behavior.
 var interp = require('../interp');
 var Bub = require('./bub');
 
